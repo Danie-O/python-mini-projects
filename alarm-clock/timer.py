5bfd132e111d6e80a03f950479f64b0bfddd61b9
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-05-16 19:29:49 +0000

    Push changes

--- alarm-clock/timer.py
+++ alarm-clock/timer.py
@@ -1,12 +1,37 @@
 # Alarm sound can be downloaded from: https://www.fesliyanstudios.com/royalty-free-sound-effects-download/alarm-203
 
 # import dependencies
-import playsound
+from playsound import playsound
 import time
+from datetime import date, datetime as dt
 
-def sound_alarm(seconds: int):
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
+def find_timedelta(alarm_time: str) -> int:
+    current_time = dt.now()
+    todays_date = date.today().strftime("%Y-%m-%d")
+    alarm_time = " ".join([todays_date, alarm_time])
+
+    deadline = dt.strptime(alarm_time, "%Y-%m-%d %H:%M:%S")
+
+    delta = deadline - current_time
+    seconds = int(delta.total_seconds())
+    return seconds
+
+# find_timedelta("19:30:00")
+
+def set_alarm(by_seconds: bool=False, by_time: bool=False):
     time_elapsed = 0
+    print(CLEAR)
 
+    if by_seconds:
+        seconds = int(input("How many seconds from now do you want to set alarm: ")) 
+    elif by_time:
+        deadline_by_time = input("What time do you want to set an alarm for today: (HH:MM:SS) eg. 19:23:00 == 7:30pm    ")
+        seconds = find_timedelta(deadline_by_time)
+
+    print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed += 1
@@ -15,6 +40,9 @@ def sound_alarm(seconds: int):
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
-        print(f"{minutes_left:02d}:{seconds_left:02d}")
+        print(f"{CLEAR_AND_RETURN} Alarm would sound in: {minutes_left:02d}:{seconds_left:02d}")
+    playsound("alarm.mp3")
 
-sound_alarm(5)
+
+set_alarm(by_time=True)
+# set_alarm(by_seconds=True)
