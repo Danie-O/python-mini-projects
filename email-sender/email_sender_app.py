f58cbca1210b32203c27933f1fd2d41f3b5882ec
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-05-20 16:56:55 +0000

    Update email_sender_app.py

--- email-sender/email_sender_app.py
+++ email-sender/email_sender_app.py
@@ -1,19 +1,23 @@
-""" STEPS:
-    - go over to gmail account and set up 2-factor authentication
-    - generate app password
-    - create a function to send email
-"""
 from email.message import EmailMessage
 from password import PASSWORD
+import ssl
+import smtplib
+
+""" STEPS:
+    - go to gmail account and set up 2-factor authentication
+    - generate app password (app= Python)
+    - create a function to send email
+"""
 
 email_sender = 'daniellaomenogor@gmail.com'
 email_password = PASSWORD
 
+# generate temporary receiver email via temp-mail.org to test the email sender
 email_receiver = ''
 subject = 'A message from [Daniella]'
 body = """
 I just built an email sender with Python, visit my github to check it out!
-Github repo link: 
+Github repo link: https://github.com/Danie-O/python-mini-projects/tree/main/email-sender
 """
 
 # define parameters for EmailMessage
@@ -22,3 +26,8 @@ em['From'] = email_sender
 em['To'] = email_receiver
 em['subject'] = subject
 em.set_content(body)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string)
